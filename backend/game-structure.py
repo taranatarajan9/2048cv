20fdef3dd1384e56639c3c54989b99c86972d74c
Tara Natarajan <taranatarajan9@gmail.com>
2021-07-21 21:56:53 -0700

    updated the game logic

--- backend/game-structure.py
+++ backend/game-structure.py
@@ -1,4 +1,4 @@
-import directions 
+from directions import Direction
 import random
 
 class GameBoard:
@@ -12,45 +12,64 @@ class GameBoard:
                 self.board[row].append(0)
         
     def move(self, direction): # function to modify board however the player wants to move
-        if direction == directions.Direction.up:
-            
-                    # update the board to move the lower spaces up one space 
+        if direction == Direction.up:
+            for row in range (self.length - 1):
+                for col in range (self.length):
+                    move_curr(self, direction, row, col)# update the board to move the lower spaces up one space 
+
+        if direction == Direction.down:
+            for row in reversed(range(1, self.length)):
+                for col in range(self.length):
+                    move_curr(self, direction, row, col)
+
+        if direction == Direction.left:
+            for col in (range(1, self.length)):
+                for row in range(self.length):
+                    move_curr(self, direction, row, col)
+
+        if direction == Direction.right:
+            for col in reversed(range(1, self.length)):
+                for row in range(self.length):
+                    move_curr(self, direction, row, col)
+
+        return True
+
 
     def move_curr(self, direction, current_y, current_x): # helper function for when two blocks are added
-        if direction == directions.Direction.up:
+        if direction == Direction.up:
             for y_coord in range(current_y + 1, self.length-1):
                 self.board[y_coord][current_x] = self.board[y_coord+1][current_x]
             self.board[self.length-1][current_x] = 0
 
-        if direction == directions.Direction.down:
+        if direction == Direction.down:
             for y_coord in reversed(range(1, current_y - 1)):
                 self.board[y_coord][current_x] = self.board[y_coord-1][current_x]
             self.board[1][current_x] = 0
         
-        if direction == directions.Direction.right:
+        if direction == Direction.right:
             for x_coord in range(current_x + 1, self.length - 1):
                 self.board[current_y][x_coord] = self.board[current_y][x_coord + 1]
             self.board[current_y][self.length - 1] = 0
         
-        if direction == directions.Direction.left:
+        if direction == Direction.left:
             for x_coord in reversed(range(1, current_x - 1)):
                 self.board[current_y][x_coord] = self.board[current_y][x_coord - 1]
             self.board[current_y][1] = 0
     
     def combine(self, direction, current_x, current_y): # helper function to check if there are two tiles to be combined
-        if direction == directions.Direction.up and current_y < self.length - 1:
+        if direction == Direction.up and current_y < self.length - 1:
             if (self.board[current_y][current_x] == self.board[current_y + 1][current_x]):
                 self.board[current_y][current_x] *= 2
         
-        if direction == directions.Direction.down and current_y > 0:
+        if direction == Direction.down and current_y > 0:
             if self.board[current_y][current_x] == self.board[current_y - 1][current_x]:
                 self.board[current_y][current_x] *= 2
         
-        if direction == directions.Direction.left and current_x > 0:
+        if direction == Direction.left and current_x > 0:
             if (self.board[current_x] == self.board[current_x - 1]):
                 self.board[current_y][current_x] *= 2
         
-        if direction == directions.Direction.right and current_x < self.length - 1:
+        if direction == Direction.right and current_x < self.length - 1:
             if self.board[current_y][current_x] == self.board[current_y][current_x + 1]:
                  self.board[current_y][current_x] *= 2
 
