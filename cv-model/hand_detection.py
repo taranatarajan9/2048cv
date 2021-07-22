b36ac029cbbd72259261ceb37157fb2c20303a10
Tara Natarajan <taranatarajan9@gmail.com>
2021-07-21 22:24:05 -0700

    restructured direction names

--- cv-model/hand_detection.py
+++ cv-model/hand_detection.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-from backend.directions import Direction
+from logic.directions import Direction
 
 def capture_video():
     cam = cv.VideoCapture(0) # accesses webcam
